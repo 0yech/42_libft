5b00ce7b0e0b5aadf43ffc84328f5584acad1987
oyech <Nelson.rey02@gmail.com>
2024-10-01 18:43:37 +0200

    end of day1

---
+++ ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrey <marvin@42lausanne.ch>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 17:35:19 by nrey              #+#    #+#             */
+/*   Updated: 2024/10/01 18:43:25 by nrey             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char *srdst = (unsigned char *)dst;
+	const unsigned char *srccpy = (const unsigned char *)src;
+	
+	if (srdst == 0 || srccpy == 0)
+		return (0);
+	if (srccpy < srdst && srdst < srccpy + n)
+	{
+		srdst += n;
+		srccpy += n;
+		while (n--)
+		{
+			*(--srdst) = *(--srccpy);
+		}
+	}
+	else
+	{
+		while(n--)
+			*srdst++ = *srccpy++;
+	}
+	return (dst);
+}
