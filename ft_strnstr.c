393e9ba6ab7f653b1bcbf50b665c46c38c4db779
oyech <Nelson.rey02@gmail.com>
2024-10-11 17:36:47 +0200

    mac fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 01:07:14 by nrey              #+#    #+#             */
-/*   Updated: 2024/10/10 20:36:41 by nrey             ###   ########.fr       */
+/*   Updated: 2024/10/11 17:32:18 by nrey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	if (len == 0 || *haystack == '\0')
-		return (0);
-	while (i < len)
+	while (i < len && *haystack)
 	{
 		j = 0;
-		while (haystack[i + j] && haystack[i + j] == needle[j] && (i + j) < len)
-		{
-			if (needle[j + 1] == '\0')
-				return ((char *)&haystack[i]);
+		while (haystack[j] && haystack[j] == needle[j] && (i + j) < len)
 			j++;
-		}
+		if (!needle[j])
+			return ((char *)haystack);
+		haystack++;
 		i++;
 	}
 	return (0);
